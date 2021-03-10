4f3a630955b5a142fcd3bd670f0ed3af6ceca985
John Dowson <dowson.john.pony@gmail.com>
2021-03-10 17:10:38 +0300

    Added rotation on all axes

--- PreciseRotation.cs
+++ PreciseRotation.cs
@@ -1,6 +1,5 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using DebugUtils;
 using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +8,23 @@ using UnityEngine;
 
 namespace PreciseRotation
 {
-    [BepInPlugin("com.github.johndowson.PreciseRotation", "PreciseRotation", "1.0.1")]
+    [BepInPlugin("com.github.johndowson.PreciseRotation", "PreciseRotation", "2.0.0")]
     public class PreciseRotation : BaseUnityPlugin
     {
         public static ConfigEntry<int> RotationSteps;
         public static ConfigEntry<bool> OverrideRotation;
         public static ConfigEntry<KeyCode> RotationModifier;
-        public static readonly string ZRotationButtonName = "PreciseRotationModifier";
+        public static ConfigEntry<KeyCode> NextAxis;
+
+        public static bool Precision;
+        public static int RotationX, RotationY, RotationZ = 0;
+        public static Axis CurrentAxis = Axis.Y;
+        public enum Axis
+        {
+            X,
+            Y,
+            Z,
+        }
 
         private static readonly Harmony harmony = new(typeof(PreciseRotation).GetCustomAttributes(typeof(BepInPlugin), false)
             .Cast<BepInPlugin>()
@@ -25,9 +34,10 @@ namespace PreciseRotation
         private void Awake()
         {
             RotationModifier = Config.Bind("General", "RotationModifier", KeyCode.LeftAlt, "Key to toggle precise rotation");
-            OverrideRotation = Config.Bind("General", "OverrideRotation", false, "Key to toggle precise rotation");
+            OverrideRotation = Config.Bind("General", "OverrideRotation", false, "Override rotation steps without the need to hold a button.");
             RotationSteps = Config.Bind("General", "RotationSteps", 16, "Number of rotation steps per 180 degrees, Valheim's default is 8");
-            ZInput.instance.AddButton(ZRotationButtonName, RotationModifier.Value);
+            NextAxis = Config.Bind("General", "NextAxis", KeyCode.V, "Key to toggle rotation axis");
+            Precision = OverrideRotation.Value;
             harmony.PatchAll();
         }
 
@@ -36,10 +46,13 @@ namespace PreciseRotation
             harmony.UnpatchSelf();
         }
 
-        [HarmonyPatch(typeof(Player), "UpdatePlacementGhost")]
+        [HarmonyPatch(typeof(Player))]
         public static class PieceRotation_Patch
         {
-            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+
+            [HarmonyPatch("UpdatePlacementGhost")]
+            [HarmonyTranspiler]
+            static IEnumerable<CodeInstruction> UpdatePlacementGhostPatch(IEnumerable<CodeInstruction> instructions)
             {
                 var codes = new List<CodeInstruction>(instructions);
                 for (var i = 0; i < codes.Count; i++)
@@ -55,23 +68,87 @@ namespace PreciseRotation
                         codes[i + 8].opcode == OpCodes.Call)
 
                     {
-                        if (OverrideRotation.Value)
-                            codes[i + 2].operand = 180.0f / RotationSteps.Value;
-                        else
-                            codes[i + 2] = CodeInstruction.Call(typeof(PieceRotation_Patch), "GetRotationSteps");
+                        for (int y = 1; y < 8; y++)
+                            codes[i + y].opcode = OpCodes.Nop;
+                        codes[i + 8] = CodeInstruction.Call(typeof(PieceRotation_Patch), "Rotate");
                     }
                 }
                 return codes.AsEnumerable();
             }
+            [HarmonyPatch("UpdatePlacement")]
+            [HarmonyPrefix]
+            static void RememberOldRotation(Player __instance, bool takeInput, float dt, ref int __state)
+            {
+                __state = __instance.m_placeRotation;
+            }
+
+            [HarmonyPatch("UpdatePlacement")]
+            [HarmonyPostfix]
+            static void StoreRotationDifference(Player __instance, bool takeInput, float dt, ref int __state)
+            {
+                int difference = -(__state - __instance.m_placeRotation);
+                switch (CurrentAxis)
+                {
+                    case Axis.X:
+                        RotationX += difference;
+                        break;
+                    case Axis.Y:
+                        RotationY += difference;
+                        break;
+                    case Axis.Z:
+                        RotationZ += difference;
+                        break;
+                }
+            }
+
+            [HarmonyPatch("Update")]
+            [HarmonyPostfix]
+            static void UpdateRotationAxis(Player __instance)
+            {
+                if (Input.GetKeyDown(NextAxis.Value))
+                    ToggleAxis();
+            }
+            [HarmonyPatch("Update")]
+            [HarmonyPostfix]
+            static void UpdatePrecision(Player __instance)
+            {
+                if (!OverrideRotation.Value && Input.GetKeyDown(RotationModifier.Value))
+                {
+                    Precision = !Precision;
+                    Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, "Pecision toggled: " + Precision, 0, null);
+                }
+            }
+
+
+            public static Quaternion Rotate()
+            {
+                var rotationSteps = GetRotationSteps();
+                return Quaternion.Euler(rotationSteps * (float)RotationX, rotationSteps * (float)RotationY, rotationSteps * (float)RotationZ);
+
+            }
             public static float GetRotationSteps()
             {
-                bool rotationModKeyHeld = ZInput.GetButton(ZRotationButtonName);
-
                 float rotationPerStep = 180.0f / 8;
-                if (rotationModKeyHeld)
+                if (Precision)
                     rotationPerStep = 180.0f / RotationSteps.Value;
-                
+
                 return rotationPerStep;
+            }
+            public static void ToggleAxis()
+            {
+                switch (CurrentAxis)
+                {
+                    case Axis.X:
+                        CurrentAxis = Axis.Y;
+                        break;
+                    case Axis.Y:
+                        CurrentAxis = Axis.Z;
+                        break;
+                    case Axis.Z:
+                        CurrentAxis = Axis.X;
+                        break;
+                }
+                Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, "Rotation toggled: " + CurrentAxis, 0, null);
             }
         }
     }
